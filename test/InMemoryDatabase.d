b0c55b59f21739742821f4ff4094ddbce1285007
Johan Ju <johanj93@gmail.com>
2016-03-24 14:15:19 +0100

    clean

--- test/InMemoryDatabase.d
+++
@@ -1,2 +0,0 @@
-InMemoryDatabase.o InMemoryDatabase.d : InMemoryDatabase.cc InMemoryDatabase.h database.h \
- newsGroup.h article.h
--- test/testSendList.d
+++
@@ -1,2 +0,0 @@
-testSendList.o testSendList.d : testSendList.cc ../connection.h \
- ../connectionclosedexception.h ../protocol.h
