aec05a0e5bdc353c8cb1d34368798214aee2fc74
elt12cry <elt12cry@student.lu.se>
2016-03-24 14:11:13 +0100

    Added Folder

---
+++ Database test/InMemoryDatabase.h
@@ -0,0 +1,51 @@
+/*
+ * InMemoryDatabase.h
+ *
+ *  Created on: Mar 22, 2016
+ *      Author: carl
+ */
+
+#ifndef TEST_INMEMORYDATABASE_H_
+#define TEST_INMEMORYDATABASE_H_
+
+#include "database.h"
+#include <vector>
+#include "newsGroup.h"
+#include "article.h"
+#include <algorithm>
+
+namespace std {
+
+class InMemoryDatabase: public Database {
+
+public:
+	InMemoryDatabase();
+	~InMemoryDatabase();
+
+	bool createNewsgroup(const std::string& newsGroupName);
+
+	bool deleteNewsgroup(const int& newsGroupId);
+
+	std::string listAllNewsgroups();
+
+	bool createArticle(const int& newsGroupId, std::string title, std::string author, std::string text);
+
+	bool deleteArticle(const int& newsGroupId, const int& artId);
+
+	std::string listArticle(const int& newsGroupId);
+
+	std::string getArticle(const int& newsGroupId, const int& artId);
+
+	bool standardCheck(const int& newsGroupId);
+
+private:
+	vector<NewsGroup> newsGroups;
+
+
+};
+
+
+
+} /* namespace std */
+
+#endif /* TEST_INMEMORYDATABASE_H_ */
---
+++ Database test/main.cc
@@ -0,0 +1,16 @@
+
+
+
+#include "database.h"
+#include "newsGroup.h"
+#include "article.h"
+#include<string>
+#include "InMemoryDatabase.h"
+
+int main(){
+
+	string newsName = "Trump";
+
+
+
+}
